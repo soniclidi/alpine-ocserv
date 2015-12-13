f5e4653b1062af794adcf177e37d1ac0a3174997
soniclidi <soniclidi@gmail.com>
2015-12-13 17:40:13 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -66,9 +66,9 @@ iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 # Enable TUN device
-#mkdir -p /dev/net
-#mknod /dev/net/tun c 10 200
-#chmod 600 /dev/net/tun
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
 
 # Run OpennConnect Server
 exec "$@"
